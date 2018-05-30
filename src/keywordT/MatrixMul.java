0d564410106d8c3eb7d582f03299d6ac3aec7483
Aochi <shuaochisoft@yahoo.co.jp>
2018-05-30 12:37:25 +0900

    little change in 5/29

--- src/keywordT/MatrixMul.java
+++ src/keywordT/MatrixMul.java
@@ -1,20 +1,21 @@
 package keywordT;
 
+import java.math.BigDecimal;
 import java.util.Random;
 
 public class MatrixMul {
 	public static void main(String[] args) {
-		int size = 2048;
-		float A[][]= new float[size][size];
-		float B[][]= new float[size][size];
-		float C[][] = new float[size][size];
-		float D[][] = new float[size][size];
+		int size = 1024;
+		int A[][]= new int[size][size];
+		int B[][]= new int[size][size];
+		int C[][] = new int[size][size];
+		int D[][] = new int[size][size];
 		Random rand = new Random();
 		long start = System.currentTimeMillis();
 		for(int i = 0; i<size ; i++ ) {
 			for(int j=0; j<size ; j++) {
-				A[i][j] = rand.nextFloat();
-				B[i][j] = rand.nextFloat();
+				A[i][j] = rand.nextInt(30);
+				B[i][j] = rand.nextInt(30);
 				C[i][j] = 0;
 			}
 		}
@@ -26,14 +27,14 @@ public class MatrixMul {
 //			System.out.println();
 		}
 		
-		System.out.println("B matrix is :");
+//		System.out.println("B matrix is :");
 		for(int i=0;i<size;i++) {
 			for(int j = 0 ;j<size ; j++) {
 //				System.out.print(B[i][j]+"   ");
 			}
 //			System.out.println();
 		}
-		System.out.println("final matrix C is :");
+//		System.out.println("final matrix C is :");
 		for (int k = 0; k<size ; k++) {
 			for(int i=0; i<size ; i++) {
 				float r = A[i][k];
@@ -50,54 +51,96 @@ public class MatrixMul {
 //			System.out.println();
 		}
 		
-		System.out.println("Test :");
-		
-		System.out.println("Method 2 with using blocking");
+//		System.out.println("Test :");
+//		
+//		System.out.println("Method 2 with using blocking");
 		int  block = 8;
 		int  nr = size/block;
-		float[][] Cc = new float[nr][nr];
-		float[][] Ac = new float[nr][nr];
-		float[][] Bc = new float[nr][nr];
-		for(int jc = 0 ; jc < size ; jc += nr) {
-			for (int kc = 0; kc < size ; kc +=  nr) {
-				for (int k = 0 ; k<nr;k++) {
-					for (int j = 0 ; j < nr ; j++) {
-						Bc[k][j] = B[k+kc][j+jc];
+		int[][] Cc = new int[nr][nr];
+		int[][] Ac = new int[nr][nr];
+		int[][] Bc = new int[nr][nr];
+		
+		
+		// new one
+		for(int ic = 0; ic<size ; ic+=nr) {
+			for(int kc = 0; kc<size ; kc +=nr) {
+				for(int i = 0; i<nr ;i++) {
+					for(int k = 0; k<nr ; k++) {
+						Ac[i][k] = A[i+ic][k+kc];
 					}
 				}
-				for (int ic = 0 ; ic < size ; ic += nr) {
-					for(int i = 0; i < nr ; i++) {
-						for(int k = 0 ; k < nr ; k++) {
-							Ac[i][k] = A[i+ic][k+kc];
+				
+				for(int jc = 0; jc<size ; jc+=nr) {
+					for(int j=0;j<nr;j++) {
+						for(int k=0;k<nr;k++) {
+							Bc[k][j] = B[k+kc][j+jc];
 						}
-						for(int j = 0; j<nr ; j++) {
+						for(int i=0;i<nr;i++) {
 							Cc[i][j] = 0;
 						}
 					}
-						for(int ir = 0; ir < nr;ir++) {
-							for(int kr = 0 ; kr<nr;kr++) {
-								float ac = Ac[ir][kr];
-								for(int jr =0;jr < nr ; jr++) {
-									Cc[ir][jr] += ac*Bc[kr][jr];
-								}
-							}
-						}
-						for(int i = 0 ; i < nr ; i++) {
-							for (int j =0 ; j < nr ; j++) {
-								D[i+ic][j+jc] += Cc[i][j];
-							}
-						}
 					
+					for(int ir = 0; ir < nr;ir++) {
+						for(int kr = 0 ; kr<nr;kr++) {
+							float ac = Ac[ir][kr];
+							for(int jr =0;jr < nr ; jr++) {
+								Cc[ir][jr] += ac*Bc[kr][jr];
+							}
+						}
+					}
+					for(int i = 0 ; i < nr ; i++) {
+						for (int j =0 ; j < nr ; j++) {
+							D[i+ic][j+jc] += Cc[i][j];
+						}
+					}
+
 				}
 			}
 		}
+//		
+		
+//		for(int jc = 0 ; jc < size ; jc += nr) {
+//			for (int kc = 0; kc < size ; kc +=  nr) {
+//				for (int k = 0 ; k<nr;k++) {
+//					for (int j = 0 ; j < nr ; j++) {
+//						Bc[k][j] = B[k+kc][j+jc];
+//					}
+//				}
+//				for (int ic = 0 ; ic < size ; ic += nr) {
+//					for(int i = 0; i < nr ; i++) {
+//						for(int k = 0 ; k < nr ; k++) {
+//							Ac[i][k] = A[i+ic][k+kc];
+//						}
+//						for(int j = 0; j<nr ; j++) {
+//							Cc[i][j] = 0;
+//						}
+//					}
+//						for(int ir = 0; ir < nr;ir++) {
+//							for(int kr = 0 ; kr<nr;kr++) {
+//								float ac = Ac[ir][kr];
+//								for(int jr =0;jr < nr ; jr++) {
+//									Cc[ir][jr] += ac*Bc[kr][jr];
+//								}
+//							}
+//						}
+//						for(int i = 0 ; i < nr ; i++) {
+//							for (int j =0 ; j < nr ; j++) {
+//								D[i+ic][j+jc] += Cc[i][j];
+//							}
+//						}
+//					
+//				}
+//			}
+//		}
 		long end2 = System.currentTimeMillis();
+		
+		
 		System.out.println("time duration 1 cost : " + (end1 - start));
 		System.out.println("time duration 2 cost : " + (end2 - end1));
-		float error = 0;
+		int error = 0;
 		for(int i= 0 ; i<size ; i++) {
 			for(int j= 0;j<size ;j ++) {
-				error += Math.abs(D[i][j] - C[i][j]);
+				error += Math.abs(D[i][j]-C[i][j]);
 			}
 			
 		}
